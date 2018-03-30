0db39cba47d82abd625759adfb0bc32c3d8c50dc
e1560762 <e1560762@ceng.metu.edu.tr>
2018-03-30 00:16:41 -0700

    Values variable is converted to string.

--- src/password_generator.py
+++ src/password_generator.py
@@ -1,12 +1,13 @@
 import random
-from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+from string import ascii_lowercase, ascii_uppercase, digits
 class PasswordGenerator(object):
-
+	def __init__(self):
+		self.punctuation = "!#$+,-._:;?@"
 	def generate(self, length=8, **kwargs):
 		min_occurrence, min_length = None, 0
 		alphabet = {}
 
-		if kwargs.has_key('min'):
+		if 'min' in kwargs:
 			min_occurrence = kwargs['min']
 		if kwargs.get('uppercase', True):
 			alphabet["u"] = ascii_uppercase
@@ -21,19 +22,19 @@ class PasswordGenerator(object):
 			if min_occurrence:
 				min_length += len(alphabet['d'])
 		if kwargs.get('punctuation', True):
-			alphabet["p"] = punctuation
+			alphabet["p"] = self.punctuation
 			if min_occurrence:
 				min_length += len(alphabet['p'])
 		if length < min_length:
 			return None, "Given length is less than minimum length"
 		password = ""
-		values = alphabet.values()
+		values = "".join(list(alphabet.values()))
 		if min_occurrence:
 			for k in alphabet:
 				password = "".join([password, random.sample(alphabet[k], min_occurrence)])
 				password = "".join(random.sample(password, len(password)))
 		else:
-			password = random.sample(values, length)
+			password = "".join(random.sample(values, length))
 		while len(password) < length:
 			password = "".join([password, random.choice(values)])
 			password = "".join(random.sample(password, len(password)))
