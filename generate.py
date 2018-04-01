78e1185e6fcab54a7ec958fd3ce4d7ff3cc31de4
e1560762 <e1560762@ceng.metu.edu.tr>
2018-03-31 18:28:46 -0700

    Command line execution is moved into separate file. GUI is added.

--- generate.py
+++ generate.py
@@ -1,30 +1,67 @@
 from src import password_generator
-import argparse
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+from kivy.base import runTouchApp
+
+class RootWidget(GridLayout):
+	def __init__(self, **kwargs):
+		super(RootWidget, self).__init__(**kwargs)
+		self.alphabet = ["lowercase", "uppercase", "digit", "punctuation"]
+		self.add_widget(Label(text="Length"))
+		txtinput = TextInput(hint_text="Enter the length of password", input_filter='int', multiline=False)
+		txtinput.bind(text=lambda ins, val: setattr(self,"pass_length", val))
+		self.add_widget(txtinput)
+		for v in self.alphabet:
+			self.add_widget(Label(text="Include %s" % (v)))
+			spinner = Spinner(
+				text="Include",
+				values=("Include","Exclude"),
+				)
+			spinner.bind(text=lambda s,t: setattr(self,"%s_exists" % (v),True if t.find("Include") > -1 else False))
+			self.add_widget(spinner)
+		self.add_widget(Label(text="Minimum Occurrence"))
+		txtinput = TextInput(hint_text="Restricts number of occurrences of each set", input_filter='int', multiline=False)
+		txtinput.bind(text=lambda ins, val: setattr(self,"min_occurrence", val))
+		self.add_widget(txtinput)
+		single_layout = GridLayout(cols=1, row_force_default = True, row_default_height=44)
+		self.add_widget(single_layout)
+		button = Button(text="Submit", size_hint_y=None, height=44)
+		button.bind(on_release=self.submit)
+		self.add_widget(button)
+
+	def submit(self, btn):
+		try:
+			length = int(getattr(self,"pass_length",None))
+		except ValueError:
+			length = 8
+
+		kwargs = {}
+		try:
+			kwargs['min'] = int(getattr(self,"min_occurrence", None))
+		except:
+			kwargs['min'] = None
+
+		for v in self.alphabet:
+			key = "%s_exists" % (v)
+			kwargs[v] = getattr(self, key, True)
+
+		pg = password_generator.PasswordGenerator()
+		result, message = pg.generate(length, **kwargs)
+		label = "Success"
+		if message:
+			result = message
+			label = "Fail"	
+		popup = Popup(title=label, content=Label(text=result), size_hint=(None, None), size=(300,100))
+		popup.open()
+
+class MainApp(App):
+	def build(self):
+		return RootWidget(cols=2, row_force_default = True, row_default_height=44)
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description="Generates random passwords for a more secure world.")
-	parser.add_argument("-L", help="Length of the password", dest="length", default=8, type=int)
-	parser.add_argument("-u", help="Uppercase letters exist?", dest="uppercase_exists", default="true", choices=["true","false"])
-	parser.add_argument("-l", help="Lowercase letters exist?", dest="lowercase_exists", default="true", choices=["true","false"])
-	parser.add_argument("-d", help="Digits exist?", dest="digit_exists", default="true", choices=["true","false"])
-	parser.add_argument("-p", help="Punctuation exist?", dest="punctuation_exists", default="true", choices=["true","false"])
-	parser.add_argument("-min", help="Minimum number of occurrence of character sets", dest="min_occurrence", default=None, type=int)
-	
-	parsed = parser.parse_args()
-	if parsed.length < 1:
-		print("Length should be at least 1")
-	if parsed.min_occurrence and parsed.min_occurrence < 1:
-		print("There should be at least one character in each set you pick.")
-	kwargs = {}
-	kwargs['min'] = parsed.min_occurrence
-	kwargs['uppercase'] = True if parsed.uppercase_exists == "true" else False
-	kwargs['lowercase'] = True if parsed.lowercase_exists == "true" else False
-	kwargs['digits'] = True if parsed.digit_exists == "true" else False
-	kwargs['punctuation'] = True if parsed.punctuation_exists == "true" else False
-
-	pg = password_generator.PasswordGenerator()
-	result, message = pg.generate(parsed.length, **kwargs)
-	if message:
-		print(message)
-	else:
-		print(result) 
+	MainApp().run()
