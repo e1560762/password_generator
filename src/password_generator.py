c9bade89bcdc3b8cc4da3160455ebc951dae8717
e1560762 <e1560762@ceng.metu.edu.tr>
2018-03-31 18:29:30 -0700

    Fixed minimum length check.

--- src/password_generator.py
+++ src/password_generator.py
@@ -12,26 +12,26 @@ class PasswordGenerator(object):
 		if kwargs.get('uppercase', True):
 			alphabet["u"] = ascii_uppercase
 			if min_occurrence:
-				min_length += len(alphabet['u'])
+				min_length += min_occurrence
 		if kwargs.get('lowercase', True):
 			alphabet["l"] = ascii_lowercase
 			if min_occurrence:
-				min_length += len(alphabet['l'])
+				min_length += min_occurrence
 		if kwargs.get('digits', True):
 			alphabet["d"] = digits
 			if min_occurrence:
-				min_length += len(alphabet['d'])
+				min_length += min_occurrence
 		if kwargs.get('punctuation', True):
 			alphabet["p"] = self.punctuation
 			if min_occurrence:
-				min_length += len(alphabet['p'])
+				min_length += min_occurrence
 		if length < min_length:
 			return None, "Given length is less than minimum length"
 		password = ""
 		values = "".join(list(alphabet.values()))
 		if min_occurrence:
 			for k in alphabet:
-				password = "".join([password, random.sample(alphabet[k], min_occurrence)])
+				password = "".join([password] + random.sample(alphabet[k], min_occurrence))
 				password = "".join(random.sample(password, len(password)))
 		else:
 			password = "".join(random.sample(values, length))
